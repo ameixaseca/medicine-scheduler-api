467b75b26761f2b78a9963e9024a8a5c9af7af4a
lindennerd <lindennerd@gmail.com>
2026-03-21 13:22:19 -0300

    fix: evaluate DateOfBirth upper bound per-request in PatientRequestValidator

--- src/MedicineScheduler.Api/Validators/PatientRequestValidator.cs
+++ src/MedicineScheduler.Api/Validators/PatientRequestValidator.cs
@@ -9,6 +9,8 @@ public class PatientRequestValidator : AbstractValidator<PatientRequest>
     public PatientRequestValidator()
     {
         RuleFor(x => x.Name).NotEmpty().MaximumLength(200);
-        RuleFor(x => x.DateOfBirth).NotEmpty().LessThan(DateOnly.FromDateTime(DateTime.Today));
+        RuleFor(x => x.DateOfBirth).NotEmpty()
+            .Must(d => d < DateOnly.FromDateTime(DateTime.Today))
+            .WithMessage("Date of birth must be in the past.");
     }
 }
